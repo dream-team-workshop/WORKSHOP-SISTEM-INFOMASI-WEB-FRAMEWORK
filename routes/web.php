5c071b2c7003fc6901c896c7b312efa03b853a73
Dimas Mulya <dimasgans39@gmail.com>
2022-04-03 11:55:45 +0700

    Menambah Banyak Fitur

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
+       Route::name('dashboard.')->prefix('dashboard')->group(function () {
+           Route::get('/', [DashboardController::class, 'index'])->name('index');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+           Route::middleware(['admin'])->group(function () {
+                Route::resource('category', ProductCategoryController::class);
+                Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only([
+                    'index', 'create', 'store', 'destroy'
+                ]);
+           });
+       });
+});
